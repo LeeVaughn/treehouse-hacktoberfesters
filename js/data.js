f8baf36f5da0552c4e9161aa497c5f79ecfdd280
LeeVaughn <dlvprogramming@gmail.com>
2020-10-02 16:09:57 -0700

    Add data for dlv

--- js/data.js
+++ js/data.js
@@ -1,14 +1,11 @@
-// objects in data array should look like this:
-// {
-// 	"url": "https://teamtreehouse.com/dlv.json",
-// 	"name": "Lee Vaughn",
-// 	"profile_name": "dlv",
-// 	"gravatar_url": "https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg",
-// 	"points": 33766
-// }
-
 // to access Treehouse student data go to: https://teamtreehouse.com/{profile_name}.json
 
 const data = [
-
+  {
+    "url": "https://teamtreehouse.com/dlv.json",
+    "name": "Lee Vaughn",
+    "profile_name": "dlv",
+    "gravatar_url": "https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg",
+    "points": 33766
+  }
 ];
