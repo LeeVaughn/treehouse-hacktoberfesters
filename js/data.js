b13710ef76a34c88e770518ad7852c5b0e57597e
LeeVaughn <dlvprogramming@gmail.com>
2020-10-01 20:42:26 -0700

    Add initial project files

---
+++ js/data.js
@@ -0,0 +1,13 @@
+// objects in data array should look like this:
+// {
+// 	"name": "Lee Vaughn",
+// 	"profile_name": "dlv",
+// 	"gravatar_url": "https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg",
+// 	"points": 33766
+// }
+
+// to access Treehouse student data go to: https://teamtreehouse.com/{profile_name}.json
+
+const data = [
+
+];
---
+++ js/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+/**
+ Show page function - shows the selected page of students
+ @param list - List of students to be paginated
+ @param page - Page number to be shown
+*/
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9
+   const endIndex = page * 9
+   const studentList = document.querySelector(".student-list")
+
+   // removes any previously displayed students
+   studentList.innerHTML = ""
+
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         const studentItem = `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${list[i].gravatar_url}" alt="Profile Picture">
+                  <h3>${list[i].name}</h3>
+                  <span class="email">${list[i].profile_name}</span>
+               </div>
+               <div>
+                  <span class="points">Points: ${list[i].points}</span>
+               </div>
+            </li>
+         `;
+
+         studentList.insertAdjacentHTML("beforeend", studentItem);
+      }
+   }
+}
+
+/**
+ Append page links function - Create and append pagination links for list
+ @param list - List of students to be paginated
+*/
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length / 9)
+   const linkList = document.querySelector(".link-list")
+
+   linkList.innerHTML = ""
+   
+   for (let i = 1; i <= numOfPages; i++) {
+      const button = `<li><button type="button">${i}</button</li>`
+
+      linkList.insertAdjacentHTML("beforeend", button);
+   }
+
+   document.querySelector("button").className = "active";
+
+   linkList.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") {
+         document.querySelector(".active").className = "";
+         e.target.className = "active"
+
+         showPage(list, e.target.textContent)
+      }
+   });
+}
+
+showPage(data, 1);
+addPagination(data);
