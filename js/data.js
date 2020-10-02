9d86e675b55678f28aec5e83b01ad981587545ed
LeeVaughn <dlvprogramming@gmail.com>
2020-10-01 20:50:22 -0700

    Add url property to data object example

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 // objects in data array should look like this:
 // {
+// 	"url": "https://teamtreehouse.com/dlv.json",
 // 	"name": "Lee Vaughn",
 // 	"profile_name": "dlv",
 // 	"gravatar_url": "https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg",
