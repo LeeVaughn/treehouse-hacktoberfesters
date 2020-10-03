829877d8fca4ebf569efee5776d60b04d2d5c4af
Juan Lopez <juanl0215@gmail.com>
2020-10-03 06:23:19 -0400

    Added profile data

--- js/data.js
+++ js/data.js
@@ -2,10 +2,19 @@
 
 const data = [
   {
-    "url": "https://teamtreehouse.com/dlv.json",
-    "name": "Lee Vaughn",
-    "profile_name": "dlv",
-    "gravatar_url": "https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg",
-    "points": 33766
-  }
+    url: 'https://teamtreehouse.com/dlv.json',
+    name: 'Lee Vaughn',
+    profile_name: 'dlv',
+    gravatar_url:
+      'https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg',
+    points: 33766,
+  },
+  {
+    url: 'https://teamtreehouse.com/thejuanandonly.json',
+    name: 'Juan Lopez',
+    profile_name: 'thejuanandonly',
+    gravatar_url:
+      'https://uploads.teamtreehouse.com/production/profile-photos/7876462/avatar_profile-pic.jpg',
+    points: 27986,
+  },
 ];
