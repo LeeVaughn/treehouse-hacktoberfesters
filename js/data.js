7676318c7856f625ef0110ffe8a5ca9a5eebd32e
Jelena Feliciano <jfel217@gmail.com>
2020-10-07 10:38:25 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -2,10 +2,10 @@
 
 const data = [
   {
-    url: "https://teamtreehouse.com/jelena",
-    name: "Jelena Feliciano",
-    profile_name: "jelena",
-    gravatar_url: "https://uploads.teamtreehouse.com/production/profile-photos/11540774/avatar_IMG_6973.jpg",
+    url: 'https://teamtreehouse.com/jelena.json',
+    name: 'Jelena Feliciano',
+    profile_name: 'jelena',
+    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/11540774/avatar_IMG_6973.jpg',
     points: 4250
   },
   {
