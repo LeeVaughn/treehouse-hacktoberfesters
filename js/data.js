b9ca84a0df31d1863cc1a1873561590780ac2834
robertpm <grobertpm@gmail.com>
2020-10-08 13:42:19 -0700

    Updated url

--- js/data.js
+++ js/data.js
@@ -44,7 +44,7 @@ const data = [
     points: 33766,
   },
   {
-    url: 'https://teamtreehouse.com/robertmanolis.json',
+    url: 'https://teamtreehouse.com/robertpm.json',
     name: 'Robert Manolis',
     profile_name: 'robertpm',
     gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/1167622/avatar_me.jpg',
