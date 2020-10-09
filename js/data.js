7776767670319f4863fb23e251cabe8f0b38c9f5
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-10-09 10:43:38 -0700

    Reverting my commit

--- js/data.js
+++ js/data.js
@@ -1,53 +1,59 @@
 // to access Treehouse student data go to: https://teamtreehouse.com/{profile_name}.json
 
-const data = [
-  {
-    url: 'https://teamtreehouse.com/jelena.json',
-    name: 'Jelena Feliciano',
-    profile_name: 'jelena',
-    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/11540774/avatar_IMG_6973.jpg',
-    points: 4250
-  },
-  {
-    url: 'https://teamtreehouse.com/agatazurek.json',
-    name: 'Agata Zurek',
-    profile_name: 'agatazurek',
-    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/10813619/avatar_LegoAstronomer.jpg',
-    points: 26419
-  },
-  {
-    url: 'https://teamtreehouse.com/jonbascos.json',
-    name: 'Jon Bascos',
-    profile_name: 'jonbascos',
-    gravatar_url: "https://uploads.teamtreehouse.com/production/profile-photos/9498722/avatar_IMG_0125.jpg",
-    points: 11634
-  },
-  {
-    url: 'https://teamtreehouse.com/thejuanandonly.json',
-    name: 'Juan Lopez',
-    profile_name: 'thejuanandonly',
-    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/7876462/avatar_profile-pic.jpg',
-    points: 27986,
-  },
-  {
-    url: 'https://teamtreehouse.com/laureneggers.json',
-    name: 'Lauren Eggers',
-    profile_name: 'laureneggers',
-    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/10759805/avatar_0.jpg',
-    points: 17379
-  },
-  {
-    url: 'https://teamtreehouse.com/dlv.json',
-    name: 'Lee Vaughn',
-    profile_name: 'dlv',
-    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg',
-    points: 33766,
-  },
-  {
-    url: 'https://teamtreehouse.com/robertpm.json',
-    name: 'Robert Manolis',
-    profile_name: 'robertpm',
-    gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/1167622/avatar_me.jpg',
-    points: 26839
-  }
+const data = [{
+        url: 'https://teamtreehouse.com/cooperhollmaier.json',
+        name: 'Cooper Hollmaier',
+        profile_name: 'cooperhollmaier',
+        gravatar_url: 'https://secure.gravatar.com/avatar/c29a3d66740b5b90c8e7ae4a70067534?s=400&d=https%3A%2F%2Fstatic.teamtreehouse.com%2Fassets%2Fcontent%2Fdefault_avatar-ea7cf6abde4eec089a4e03cc925d0e893e428b2b6971b12405a9b118c837eaa2.png&r=pg',
+        points: 5185
+    },
+    {
+        url: 'https://teamtreehouse.com/jelena.json',
+        name: 'Jelena Feliciano',
+        profile_name: 'jelena',
+        gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/11540774/avatar_IMG_6973.jpg',
+        points: 4250
+    },
+    {
+        url: 'https://teamtreehouse.com/agatazurek.json',
+        name: 'Agata Zurek',
+        profile_name: 'agatazurek',
+        gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/10813619/avatar_LegoAstronomer.jpg',
+        points: 26419
+    },
+    {
+        url: 'https://teamtreehouse.com/jonbascos.json',
+        name: 'Jon Bascos',
+        profile_name: 'jonbascos',
+        gravatar_url: "https://uploads.teamtreehouse.com/production/profile-photos/9498722/avatar_IMG_0125.jpg",
+        points: 11634
+    },
+    {
+        url: 'https://teamtreehouse.com/thejuanandonly.json',
+        name: 'Juan Lopez',
+        profile_name: 'thejuanandonly',
+        gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/7876462/avatar_profile-pic.jpg',
+        points: 27986,
+    },
+    {
+        url: 'https://teamtreehouse.com/laureneggers.json',
+        name: 'Lauren Eggers',
+        profile_name: 'laureneggers',
+        gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/10759805/avatar_0.jpg',
+        points: 17379
+    },
+    {
+        url: 'https://teamtreehouse.com/dlv.json',
+        name: 'Lee Vaughn',
+        profile_name: 'dlv',
+        gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/9205552/avatar_lee.jpg',
+        points: 33766,
+    },
+    {
+        url: 'https://teamtreehouse.com/robertpm.json',
+        name: 'Robert Manolis',
+        profile_name: 'robertpm',
+        gravatar_url: 'https://uploads.teamtreehouse.com/production/profile-photos/1167622/avatar_me.jpg',
+        points: 26839
+    }
 ];
